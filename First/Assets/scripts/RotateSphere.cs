e6285f12f65282f3debc7f75ce3c29c768fc4741
smeerkahoven <smeerkahoven@gmail.com>
2019-11-22 01:02:58 -0400

    animation barbarian

---
+++ First/Assets/scripts/RotateSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(0,0,150);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
